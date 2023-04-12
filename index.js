19cdefa200301e111bf55a509e9920ccd5effc8a
DruckDeveloper <druckdev@gmail.com>
2023-04-11 23:54:54 -0500

    adding conversation context for better answers, last 15 messages

--- index.js
+++ index.js
@@ -26,17 +26,26 @@ client.on('messageCreate', async (message) => {
 
   const conversationLog = [{ role: 'system', content: 'Eres ChatGPT, un modelo de lenguaje grande entrenado por OpenAI. Sigue las instrucciones del usuario cuidadosamente. ' }]
 
-  conversationLog.push({
-    role: 'user',
-    content: message.content
-  })
-
   await message.channel.sendTyping()
+
+  let prevMessages = await message.channel.messages.fetch({ limit: 15 })
+  prevMessages.reverse();
+
+  prevMessages.forEach((msg) => {
+    if (message.content.startsWith('!')) return;
+    if (msg.author.id !== client.user.id && message.author.bot) return;
+    if (msg.author.id !== message.author.id) return;
+
+    conversationLog.push({
+      role: 'user',
+      content: msg.content
+    });
+  });
 
   const result = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: conversationLog
-  })
+  });
 
   message.reply(result.data.choices[0].message)
 })
