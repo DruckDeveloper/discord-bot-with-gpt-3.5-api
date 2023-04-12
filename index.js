231107489738ed229223fa6dcf3de06e849c1a0d
DruckDeveloper <druckdev@gmail.com>
2023-04-11 22:55:42 -0500

    bot can respond within an allowed channel (without conversation context)

--- index.js
+++ index.js
@@ -19,10 +19,26 @@ client.on('ready', _ => {
   console.log('The bot is runing')
 })
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
   if (message.channel.id !== process.env.DISCORD_CHANNEL_ID) return;
   if (message.content.startsWith('!')) return;
+
+  const conversationLog = [{ role: 'system', content: 'Eres ChatGPT, un modelo de lenguaje grande entrenado por OpenAI. Sigue las instrucciones del usuario cuidadosamente. ' }]
+
+  conversationLog.push({
+    role: 'user',
+    content: message.content
+  })
+
+  await message.channel.sendTyping()
+
+  const result = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: conversationLog
+  })
+
+  message.reply(result.data.choices[0].message)
 })
 
 client.login(process.env.TOKEN)
