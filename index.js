aa8f8811d02ee76d85c1f8ca7b768553370513ca
DruckDeveloper <druckdev@gmail.com>
2023-04-11 22:15:38 -0500

    OpenAI API configuration added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv/config');
 const { Client, IntentsBitField } = require('discord.js');
+const { Configuration, OpenAIApi } = require('openai')
 
 const client = new Client({
   intents: [
@@ -9,8 +10,17 @@ const client = new Client({
   ]
 })
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(configuration)
+
 client.on('ready', _ => {
   console.log('The bot is runing')
+})
+
+client.on('messageCreate', (message) => {
+  console.log(message.content)
 })
 
 client.login(process.env.TOKEN)
